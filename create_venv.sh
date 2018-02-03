6122df67b1c465ec962fcca379613c5920661f98
thomas <thomas@adress>
2018-02-03 20:16:47 +0100

    first commit

---
+++ create_venv.sh
@@ -0,0 +1,24 @@
+MODE=$1
+
+
+if [ ! -z ${MODE+x} ]; then
+    MODE=local
+fi
+
+echo $MODE
+
+
+PYTHON_VERSION="3.6.3"
+PYTHON_SOURCE=Python-${PYTHON_VERSION}
+PYTHON_BIN=${PYTHON_SOURCE}/python
+if [ ! -d "${PYTHON_SOURCE}" ]; then
+wget https://www.python.org/ftp/python/${PYTHON_VERSION}/${PYTHON_SOURCE}.tgz 
+tar xzf "${PYTHON_SOURCE}.tgz"
+cd ${PYTHON_SOURCE}
+./configure && make
+cd ..
+fi
+
+VENV=".env_${MODE}"
+${PYTHON_SOURCE}/python -m venv ${VENV}
+$VENV/bin/pip install -r requirements.txt
